925e0a577d357b1d28f80494c740cde744b23b0a
Umbraco Cloud <support@umbraco.io>
2024-02-05 23:03:14 +0000

    Deployment f5d25586-db61-43ca-b465-5972b839df22: Run number 407

--- src/WRA.Umbraco.Web/wwwroot/js/src/components/checkout.js
+++ src/WRA.Umbraco.Web/wwwroot/js/src/components/checkout.js
@@ -1,16 +1,31 @@
 const checkout = () => {
     const stateDropdown = document.querySelector('.js-checkout-state');
     const countyField = document.querySelector('.js-checkout-county');
+    
+    if (stateDropdown) {
+        stateDropdown.addEventListener('change', (e) => {
+            if (e.target.value == 'WI') {
+                countyField.classList.remove('d-none');
+            } else {
+                countyField.classList.add('d-none');
+            }
+        });
+    }
 
-    if (!stateDropdown) return;
+    ///
 
-    stateDropdown.addEventListener('change', (e) => {
-        if (e.target.value == 'WI') {
-            countyField.classList.remove('d-none');
-        } else {
-            countyField.classList.add('d-none');
-        }
-    });
+    const billingAddressRadio = document.querySelector('.js-alt-billing-address');
+    const billingAddressFields = document.querySelector('.js-alt-billing-address-fields');
+
+    if (billingAddressRadio) {
+        billingAddressRadio.addEventListener('change', (e) => {
+            if (billingAddressRadio.checked) {
+                billingAddressFields.classList.remove('d-none');
+            } else {
+                billingAddressFields.classList.add('d-none');
+            }
+        });
+    }
 }
 
 export default checkout;
