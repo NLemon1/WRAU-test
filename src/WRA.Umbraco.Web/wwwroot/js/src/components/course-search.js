d60a43c3bc2799a2930eb40d974d54866a0155dc
Umbraco Cloud <support@umbraco.io>
2024-03-07 01:06:51 +0000

    Deployment f6871b6b-db96-4c6f-a2aa-99e13349ed15: Run number 650

--- src/WRA.Umbraco.Web/wwwroot/js/src/components/course-search.js
+++ src/WRA.Umbraco.Web/wwwroot/js/src/components/course-search.js
@@ -2,157 +2,39 @@ const courseSearch = () => {
 
     //DOM presentation elements
     const resultsLoader = document.querySelector('.js-results-loader-courses');
-    const courseSearchForm = document.getElementById('course-search');
 
     //DOM event elements
-    const productCategoryDropdown = document.querySelector('.js-courses-category-filter');
-    const productSubCategoryDropdown = document.querySelector('.js-courses-subcategory-filter');
-    const productSubCategoryContainer = document.querySelector('.js-courses-subcategory-container');
+    const productCategoryDropdown = document.querySelector('.js-category-filter');
+    const productSubCategoryDropdown = document.querySelector('.js-subcategory-filter');
+    const productSubCategoryContainers = document.querySelectorAll('.js-subcategory-container');
+    const productSubmit = document.querySelector('.js-courses-submit');
 
     //Static vars
-    const actionInitial = courseSearchForm.getAttribute("action");
-    const apiEndpointUrl = "/GetProducts";
-    let pageNumber = 1;
-    const pageSize = 100;
-    let subcategoryArray = ["All"];
+   // const actionInitial = courseSearchForm.getAttribute("action");
 
-    let category = "";
-    let subcategory = ""; 
+    let activeSubCat = "";
 
-    //Results
+    productCategoryDropdown.addEventListener("change", (e) => {
 
-    const handleIndicators = (show, waitLoaderElement) => {
-        if (show == true) {
-            waitLoaderElement.hidden = false;
-        } else {
-            waitLoaderElement.hidden = true;
-        }
-    }
+        const categoryValue = e.target.value;
+        activeSubCat = "";
 
-    const renderResults = (results) => {
-
-        if (results.length > 0) {
-
-            results.forEach((result) => {
-                if (result.subCategory != "All" && !subcategoryArray.includes(result.subCategory, subcategoryArray)) {
-                    subcategoryArray.push(result.subCategory);
-                    populateSubCategories();
-                }
-            });
-        }
-
-        handleIndicators(false, resultsLoader);
-    }
-
-    const populateSubCategories = () => {
-
-        productSubCategoryDropdown.innerHTML = "";//clear <option> tags
-
-        if (subcategoryArray.length > 1 && category !== "") {
-            productSubCategoryContainer.removeAttribute("hidden");
-        } else {
-            productSubCategoryContainer.setAttribute("hidden", "hidden");
-        }
-
-        subcategoryArray.forEach((subCatOption) => {
-            productSubCategoryDropdown.innerHTML += (
-                `
-                <option value="${subCatOption}">${subCatOption}</option>
-                `
-            )
-        });
-
-    }
-
-    postResults = () => {
-
-        handleIndicators(true, resultsLoader);
-
-        let bodyObject = {
-            "productType": "Courses", //Events, Products, Courses
-            "category": category, //Professional Development, Publications, Conferences/Conventions, etc.
-            "subCategory": subcategory == "All" ? "" : subcategory,//children of category
-            "taxonomy": "",//Reference Manuals, Books, Virtual, etc.
-            "pagination": {
-                "pageNumber": pageNumber,
-                "pageSize": pageSize
+        productSubCategoryContainers.forEach((productSubCategoryContainer) => {
+            if (productSubCategoryContainer.dataset.id === categoryValue) {
+                productSubCategoryContainer.hidden = false;
+                activeSubCat = categoryValue;
+            } else {
+                productSubCategoryContainer.hidden = true;
             }
-        };
-
-        const bodyRequest = JSON.stringify(bodyObject);
-
-        fetch(apiEndpointUrl,
-        {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            },
-            referrerPolicy: "no-referrer",
-            body: bodyRequest
-        }).then(res => {
-            return res.json();
-        }).then(res => {
-            //console.log(res);
-            renderResults(res);
-        });
-    }
-
-    const resetDropdown = (dropdownElement, setValue) => {
-        dropdownElement.value = setValue;
-    }
-
-    //Event Handlers
-    if (productCategoryDropdown) {
-        productCategoryDropdown.addEventListener("change", (e) => {
-            e.preventDefault();
-            category = e.target.value;
-            //clearing subcategory dropdown
-            subcategory = "";
-            subcategoryArray = ["All"];
-            productSubCategoryContainer.setAttribute("hidden", "hidden");
-            productSubCategoryDropdown.innerHTML = "";//clear <option> tags
-
-            if (productSubCategoryDropdown) {
-                resetDropdown(productSubCategoryDropdown, "");
-            }
-
-            postResults();
         })
-    }
 
-    if (productSubCategoryDropdown) {
-
-        productSubCategoryDropdown.addEventListener("change", (e) => {
-            e.preventDefault();
-            subcategory = e.target.value;
-            postResults();
-        })
-    }
-
-    let updatedAction = false;
-
-    const updateActionUrl = () => {
-
-        const setParams = subcategory !== "" ? `${actionInitial}?type=Courses&category=${category}&subcategory=${subcategory}` : `${actionInitial}?type=Courses&category=${category}`;
-        courseSearchForm.setAttribute("action", setParams);
-
-        updatedAction = true;
-
-        if (updatedAction) {
-            courseSearchForm.submit();
+        if (activeSubCat !== "") {
+            productSubmit.disabled = false;
+        } else {
+            productSubmit.disabled = true;
         }
-    }
 
-    courseSearchForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        updateActionUrl();
-    })
-
-    //Page Load
-
-    postResults();
+    });
 
 };
 
