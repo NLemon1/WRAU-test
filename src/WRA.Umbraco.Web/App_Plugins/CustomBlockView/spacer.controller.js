50b1326315eacd66f2527f4e959b371e4ea04a59
Umbraco Cloud <support@umbraco.io>
2024-03-14 19:36:59 +0000

    Deployment b0bcf862-7a30-4fc7-b32b-a23fea0c7b63: Run number 706

---
+++ src/WRA.Umbraco.Web/App_Plugins/CustomBlockView/spacer.controller.js
@@ -0,0 +1,26 @@
+angular.module("umbraco").controller("spacerController", function ($scope) {
+    
+    const block = $scope.block.data;
+    console.log(block);
+
+    let style = "";
+
+    if (block.heightSmall) {
+        style += `height: ${block.heightSmall};`;
+    }
+
+    if (block.heightMedium) {
+        style += `height: ${block.heightMedium};`;
+    }
+
+    if (block.heightLarge) {
+        style += `height: ${block.heightLarge};`;
+    }
+
+    if (block.heightExtraLarge) {
+        style += `height: ${block.heightExtraLarge};`;
+    }
+
+    $scope.style = style;
+
+});
