c718f8393e7ab8e9a8545a45596bc766075772a1
Umbraco Cloud <support@umbraco.io>
2024-03-14 17:32:16 +0000

    Deployment 50ca5823-e651-400a-b49f-0032d48e17f7: Run number 705

--- src/WRA.Umbraco.Web/App_Plugins/CustomBlockView/genericSection.controller.js
+++ src/WRA.Umbraco.Web/App_Plugins/CustomBlockView/genericSection.controller.js
@@ -2,21 +2,32 @@ angular.module("umbraco").controller("genericSectionController", function ($scop
 
     const block = $scope.block.data;
 
-    let className = "block-generic-section";
+    let className = "block-generic-section container mx-auto";
     let style = "";
 
+    if (block.width) {
+        switch (block.width[0]) {
+            case "744px":
+                className += ` container--sm`;
+                break;
+            case "936px":
+                className += ` container--md`;
+                break;
+        }
+    }
+
     if (block.yPadding) {
-        style += `padding-bottom:${block.yPadding};`;
-        style += `padding-top:${block.yPadding};`;
+        style += ` padding-bottom: ${block.yPadding};`;
+        style += ` padding-top: ${block.yPadding};`;
     }
 
     if (block.xPadding) {
-        style += `padding-left:${block.xPadding};`;
-        style += `padding-right:${block.xPadding};`;
+        style += ` padding-left: ${block.xPadding};`;
+        style += ` padding-right: ${block.xPadding};`;
     }
 
     if (block.color) {
-        style += `background-color:#${block.color.value};`;
+        style += ` background-color: #${block.color.value};`;
     }
 
     if (block.additionalCssClasses) {
