5f2756fa243cbb779e5ed4f51e483ded9e8c0a3f
Umbraco Cloud <support@umbraco.io>
2024-03-15 21:38:41 +0000

    Deployment 96941f45-ffe6-41d8-b2dd-ead050cb7970: Run number 713

--- src/WRA.Umbraco.Web/App_Plugins/CustomBlockView/testimonialsSlider.controller.js
+++ src/WRA.Umbraco.Web/App_Plugins/CustomBlockView/testimonialsSlider.controller.js
@@ -3,7 +3,7 @@ angular.module("umbraco").controller("testimonialsSliderController", function ($
     //console.log($scope.block.data);
 
     // docs: https://github.com/umbraco/Umbraco-CMS/blob/34e80d86e8c0b754f6b7a02e307f53cb32806bbe/src/Umbraco.Web.UI.Client/src/common/resources/content.resource.js#L372
-    contentResource.getChildren(1205, { pageSize: 4, pageNumber: 1, orderDirection: 'Descending' })
+    contentResource.getChildren(1384, { pageSize: 4, pageNumber: 1, orderDirection: 'Descending' })
         .then(function (data) {
             //console.log(data);
 
