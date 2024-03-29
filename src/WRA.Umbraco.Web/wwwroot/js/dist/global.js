193e111a10b26de750d43a4cfa57656de12dda77
Umbraco Cloud <support@umbraco.io>
2024-03-29 18:46:50 +0000

    Deployment dcdd4a6f-bdd9-4021-93b1-ec4230feed97: Run number 761

--- src/WRA.Umbraco.Web/wwwroot/js/dist/global.js
+++ src/WRA.Umbraco.Web/wwwroot/js/dist/global.js
@@ -7739,8 +7739,27 @@
 
   // wwwroot/js/src/components/checkout.js
   var checkout = () => {
+    const accountStepHeading = document.querySelector(".js-account-heading");
+    const showSignIn = document.querySelector(".js-show-sign-in");
+    const showCreateAccount = document.querySelector(".js-show-create-account");
+    const signInWrapper = document.querySelector(".js-checkout-sign-in");
+    const createAccountWrapper = document.querySelector(".js-checkout-create-account");
     const stateDropdown = document.querySelector(".js-checkout-state");
     const countyField = document.querySelector(".js-checkout-county");
+    if (showSignIn) {
+      showSignIn.addEventListener("click", (e) => {
+        accountStepHeading.textContent = "Sign In";
+        signInWrapper.classList.remove("d-none");
+        createAccountWrapper.classList.add("d-none");
+      });
+    }
+    if (showCreateAccount) {
+      showCreateAccount.addEventListener("click", (e) => {
+        accountStepHeading.textContent = "Create Account";
+        signInWrapper.classList.add("d-none");
+        createAccountWrapper.classList.remove("d-none");
+      });
+    }
     if (stateDropdown) {
       stateDropdown.addEventListener("change", (e) => {
         if (e.target.value == "WI") {
