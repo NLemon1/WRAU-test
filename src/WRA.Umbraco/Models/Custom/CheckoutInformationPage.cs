0a547d031d7d2846a90bc8f274bf97e023c983c1
Umbraco Cloud <support@umbraco.io>
2024-04-12 20:48:19 +0000

    Deployment ef770c0f-86da-4992-b704-45f365f80301: Run number 816

--- src/WRA.Umbraco/Models/Custom/CheckoutInformationPage.cs
+++ src/WRA.Umbraco/Models/Custom/CheckoutInformationPage.cs
@@ -7,4 +7,5 @@ namespace WRA.Umbraco.Models;
 public partial class CheckoutInformationPage
 {
     public IEnumerable<CountryReadOnly> Countries => UmbracoCommerceApi.Instance.GetCountries(this.GetStore().Id);
+
 }
