c0eac48815d6704d49e6c7ed3606fb75287456a9
Umbraco Cloud <support@umbraco.io>
2024-04-14 19:03:10 +0000

    Deployment fc7ad42f-051b-4696-a231-a5a2ea05cbac: Run number 817

---
+++ src/WRA.Umbraco/Web/Controllers/CheckoutAccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using WRA.Umbraco.Web.Services;
+
+namespace WRA.Umbraco.Web.Controllers
+{
+    public class CheckoutAccountController : RenderController
+    {
+        private readonly IMemberManager _memberManager;
+        public CheckoutAccountController(
+        ILogger<RenderController> logger,
+        ICompositeViewEngine compositeViewEngine,
+        IUmbracoContextAccessor umbracoContextAccessor,
+        IPublishedValueFallback publishedValueFallback,
+        IMemberManager memberManager)
+        : base(logger,
+            compositeViewEngine,
+            umbracoContextAccessor)
+        {
+            _memberManager = memberManager;
+        }
+
+        public override IActionResult Index()
+        {
+            MemberIdentityUser? currentMember = _memberManager.GetCurrentMemberAsync().GetAwaiter().GetResult();
+            // if user is logged in, direct them to the next step
+            if (currentMember != null)
+            {
+                return Redirect("/checkout/customer-information");
+            }
+            return CurrentTemplate(CurrentPage);
+        }
+    }
+}
