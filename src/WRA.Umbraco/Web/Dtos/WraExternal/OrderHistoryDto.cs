0eacf5d39ea3171279dd37d1b08d35f197463050
Umbraco Cloud <support@umbraco.io>
2024-04-25 20:51:57 +0000

    Deployment ac23f4ff-ba92-45f6-94ac-3cdb0b0dc81d: Run number 856

---
+++ src/WRA.Umbraco/Web/Dtos/WraExternal/OrderHistoryDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WRA.Umbraco.Web.Dtos;
+public class OrderHistoryDto
+{
+    [JsonPropertyName("memberID")]
+    public int MemberID { get; set; }
+}
+
+public class OrderHistoryOrder
+{
+    [JsonPropertyName("orderID")]
+    public int OrderID { get; set; }
+
+    // Only if different than order ID
+    [JsonPropertyName("orderNumber")]
+    public string OrderNumber { get; set; }
+
+    [JsonPropertyName("orderDate")]
+    public DateTime OrderDate { get; set; }
+
+    [JsonPropertyName("orderTotal")]
+    public decimal OrderTotal { get; set; }
+
+    [JsonPropertyName("orderStatus")]
+    public string OrderStatus { get; set; }
+
+    //If order contains subscription
+    [JsonPropertyName("expirationDate")]
+    public DateTime? ExpirationDate { get; set; }
+}
