ec6815ed1d5469fff7bc74702a63808e5c492c6a
Umbraco Cloud <support@umbraco.io>
2024-08-16 20:27:42 +0000

    Deployment c8e473e2-64e2-476d-ab5b-380ccea837d9: Run number 1064

--- src/WRA.Umbraco.Web/build.mjs
+++ src/WRA.Umbraco.Web/build.mjs
@@ -3,6 +3,7 @@ import process from "process";
 import { sassPlugin } from 'esbuild-sass-plugin';
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
+import purgecss from 'esbuild-plugin-purgecss';
 
 const prod = (process.argv[2] === 'production');
 
@@ -19,8 +20,16 @@ let ctx = await esbuild.context({
                 const { css } = await postcss([autoprefixer]).process(source)
                 return css
             }
+        }),
+        prod && purgecss({
+            content: [
+                './wwwroot/**/*.html',
+                './wwwroot/**/*.js',
+                './wwwroot/**/*.cshtml' // Add this line to include .cshtml files
+            ],
+            safelist: ['safelist-class'], // Add any classes you want to keep here
         })
-    ],
+    ].filter(Boolean), // Remove undefined values from plugins array
     define: {
         "global": 'window',//needed for global vars
     }
