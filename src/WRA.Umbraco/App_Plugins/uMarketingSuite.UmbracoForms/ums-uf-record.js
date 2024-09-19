3a884695255a97c66fdd0c9de72dc8f163be4ae2
Umbraco Cloud <support@umbraco.io>
2024-09-19 19:10:52 +0000

    Deployment e258ab98-c47b-4970-bed7-301ed008d48a: Run number 1096

--- src/WRA.Umbraco/App_Plugins/uMarketingSuite.UmbracoForms/ums-uf-record.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module("uMarketingSuite.UmbracoForms").component("umsUfRecord", {
-    templateUrl: "/App_Plugins/uMarketingSuite.UmbracoForms/ums-uf-record.html",
-    bindings: {
-        record: "<",
-    },
-});
--- src/WRA.Umbraco/App_Plugins/uMarketingSuite/uMarketingSuite-init.js
+++
@@ -1,5 +0,0 @@
-﻿// Register with Angular
-angular.module("uMarketingSuite", ["rzSlider", "as.sortable"]);
-
-// Register as dependency for Umbraco
-angular.module("umbraco").requires.push("uMarketingSuite");
